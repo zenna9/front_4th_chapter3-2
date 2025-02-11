b9fb917768dc9327bddf997453079027de1ce891
zenna <97446606+zenna9@users.noreply.github.com>
2025-02-12 00:55:51 +0900

    :white_check_mark: test : 반복유형 선택 관련 테스트코드 작성&성공확인

--- src/__tests__/repeatEvent.textCase.spec.tsx
+++ src/__tests__/repeatEvent.textCase.spec.tsx
@@ -19,15 +19,193 @@ const setup = (element: ReactElement) => {
   return { ...render(<ChakraProvider>{element}</ChakraProvider>), user };
 };
 
+const newEvent: EventForm = {
+  title: '새 회의',
+  date: '2024-10-15',
+  startTime: '14:00',
+  endTime: '15:00',
+  description: '프로젝트 진행 상황 논의',
+  location: '회의실 A',
+  category: '업무',
+  repeat: { type: 'daily', interval: 3 },
+  notificationTime: 1,
+};
+
+const saveSchedule = async (user: UserEvent, form: Omit<Event, 'id' | 'notificationTime'>) => {
+  const {
+    title,
+    date,
+    startTime,
+    endTime,
+    location,
+    description,
+    category,
+    repeat: { type: repeatType, interval: repeatInterval },
+  } = form;
+
+  await user.click(screen.getAllByText('일정 추가')[0]);
+
+  await user.type(screen.getByLabelText('제목'), title);
+  await user.type(screen.getByLabelText('날짜'), date);
+  await user.type(screen.getByLabelText('시작 시간'), startTime);
+  await user.type(screen.getByLabelText('종료 시간'), endTime);
+  await user.type(screen.getByLabelText('설명'), description);
+  await user.type(screen.getByLabelText('위치'), location);
+  await user.selectOptions(screen.getByLabelText('카테고리'), category);
+
+  //반복설정 체크
+  const repeatCheckbox = screen.getByLabelText('반복 설정') as HTMLInputElement;
+  if (!repeatCheckbox.checked) {
+    await user.click(repeatCheckbox); // 체크가 안 되어 있으면 클릭
+  }
+
+  await user.selectOptions(screen.getByLabelText('반복 유형'), repeatType);
+  await user.clear(screen.getByLabelText('반복 간격')); //초기화를 안하니까 13이됨
+  await user.type(screen.getByLabelText('반복 간격'), `${repeatInterval}`);
+
+  await user.click(screen.getByTestId('event-submit-button'));
+};
+
 describe('(필수) 반복 유형 선택', () => {
-  it('일정 생성 시 반복 유형을 선택할 수 있다.(매일).', async () => {});
-  it('일정 생성 시 반복 유형을 선택할 수 있다.(매주).', async () => {});
-  it('일정 생성 시 반복 유형을 선택할 수 있다.(매월).', async () => {});
-  it('일정 생성 시 반복 유형을 선택할 수 있다.(매년).', async () => {});
-  it('일정 수정 시 반복 유형을 선택할 수 있다.(매일).', async () => {});
-  it('일정 수정 시 반복 유형을 선택할 수 있다.(매주).', async () => {});
-  it('일정 수정 시 반복 유형을 선택할 수 있다.(매월).', async () => {});
-  it('일정 수정 시 반복 유형을 선택할 수 있다.(매년).', async () => {});
+  it('일정 생성 시 반복 유형을 선택할 수 있다.(매일).', async () => {
+    setupMockHandlerCreation();
+
+    const { user } = setup(<App />);
+    await saveSchedule(user, newEvent);
+    const eventList = within(screen.getByTestId('event-list'));
+
+    expect(eventList.getByText('반복: 3일마다')).toBeInTheDocument();
+  });
+
+  it('일정 생성 시 반복 유형을 선택할 수 있다.(매주).', async () => {
+    setupMockHandlerCreation();
+
+    const { user } = setup(<App />);
+    const changedEvent: EventForm = { ...newEvent, repeat: { type: 'weekly', interval: 3 } };
+    await saveSchedule(user, changedEvent);
+    const eventList = within(screen.getByTestId('event-list'));
+
+    expect(eventList.getByText('반복: 3주마다')).toBeInTheDocument();
+  });
+
+  it('일정 생성 시 반복 유형을 선택할 수 있다.(매월).', async () => {
+    setupMockHandlerCreation();
+
+    const { user } = setup(<App />);
+    const changedEvent: EventForm = { ...newEvent, repeat: { type: 'monthly', interval: 3 } };
+    await saveSchedule(user, changedEvent);
+    const eventList = within(screen.getByTestId('event-list'));
+
+    expect(eventList.getByText('반복: 3월마다')).toBeInTheDocument();
+  });
+
+  it('일정 생성 시 반복 유형을 선택할 수 있다.(매년).', async () => {
+    setupMockHandlerCreation();
+
+    const { user } = setup(<App />);
+    const changedEvent: EventForm = { ...newEvent, repeat: { type: 'yearly', interval: 3 } };
+    await saveSchedule(user, changedEvent);
+    const eventList = within(screen.getByTestId('event-list'));
+
+    expect(eventList.getByText('반복: 3년마다')).toBeInTheDocument();
+  });
+
+  it('일정 수정 시 반복 유형을 선택할 수 있다.(매일).', async () => {
+    const { user } = setup(<App />);
+    setupMockHandlerUpdating();
+
+    await user.click(await screen.findByLabelText('Edit event'));
+    await user.clear(screen.getByLabelText('제목'));
+    await user.type(screen.getByLabelText('제목'), '수정된 회의');
+    //반복설정 체크
+    const repeatCheckbox = screen.getByLabelText('반복 설정') as HTMLInputElement;
+    if (!repeatCheckbox.checked) {
+      await user.click(repeatCheckbox);
+    }
+    await user.selectOptions(screen.getByLabelText('반복 유형'), 'daily');
+    await user.clear(screen.getByLabelText('반복 간격'));
+    await user.type(screen.getByLabelText('반복 간격'), `5`);
+    await user.click(screen.getByTestId('event-submit-button')); // 제출
+
+    const eventList = within(screen.getByTestId('event-list'));
+
+    expect(eventList.getByText('수정된 회의')).toBeInTheDocument();
+    expect(eventList.getByText('반복: 5일마다')).toBeInTheDocument();
+  });
+
+  it('일정 수정 시 반복 유형을 선택할 수 있다.(매주).', async () => {
+    const { user } = setup(<App />);
+    setupMockHandlerUpdating();
+
+    await user.click(await screen.findByLabelText('Edit event'));
+
+    await user.clear(screen.getByLabelText('제목'));
+    await user.type(screen.getByLabelText('제목'), '수정된 회의');
+    //반복설정 체크
+    const repeatCheckbox = screen.getByLabelText('반복 설정') as HTMLInputElement;
+    if (!repeatCheckbox.checked) {
+      await user.click(repeatCheckbox);
+    }
+    await user.selectOptions(screen.getByLabelText('반복 유형'), 'weekly');
+    await user.clear(screen.getByLabelText('반복 간격'));
+    await user.type(screen.getByLabelText('반복 간격'), `5`);
+    await user.click(screen.getByTestId('event-submit-button')); // 제출
+
+    const eventList = within(screen.getByTestId('event-list'));
+
+    expect(eventList.getByText('수정된 회의')).toBeInTheDocument();
+    expect(eventList.getByText('반복: 5주마다')).toBeInTheDocument();
+  });
+
+  it('일정 수정 시 반복 유형을 선택할 수 있다.(매월).', async () => {
+    const { user } = setup(<App />);
+
+    setupMockHandlerUpdating();
+
+    await user.click(await screen.findByLabelText('Edit event'));
+    await user.clear(screen.getByLabelText('제목'));
+    await user.type(screen.getByLabelText('제목'), '수정된 회의');
+    //반복설정 체크
+    const repeatCheckbox = screen.getByLabelText('반복 설정') as HTMLInputElement;
+    if (!repeatCheckbox.checked) {
+      await user.click(repeatCheckbox);
+    }
+    await user.selectOptions(screen.getByLabelText('반복 유형'), 'monthly');
+    await user.clear(screen.getByLabelText('반복 간격'));
+    await user.type(screen.getByLabelText('반복 간격'), `5`);
+    await user.click(screen.getByTestId('event-submit-button')); // 제출
+
+    const eventList = within(screen.getByTestId('event-list'));
+
+    expect(eventList.getByText('수정된 회의')).toBeInTheDocument();
+    expect(eventList.getByText('반복: 5월마다')).toBeInTheDocument();
+  });
+
+  it('일정 수정 시 반복 유형을 선택할 수 있다.(매년).', async () => {
+    const { user } = setup(<App />);
+
+    setupMockHandlerUpdating();
+
+    await user.click(await screen.findByLabelText('Edit event'));
+
+    await user.clear(screen.getByLabelText('제목'));
+    await user.type(screen.getByLabelText('제목'), '수정된 회의');
+    //반복설정 체크
+    const repeatCheckbox = screen.getByLabelText('반복 설정') as HTMLInputElement;
+    if (!repeatCheckbox.checked) {
+      await user.click(repeatCheckbox);
+    }
+    await user.selectOptions(screen.getByLabelText('반복 유형'), 'yearly');
+    await user.clear(screen.getByLabelText('반복 간격'));
+    await user.type(screen.getByLabelText('반복 간격'), `5`);
+    await user.click(screen.getByTestId('event-submit-button')); // 제출
+
+    const eventList = within(screen.getByTestId('event-list'));
+
+    expect(eventList.getByText('수정된 회의')).toBeInTheDocument();
+    expect(eventList.getByText('반복: 5년마다')).toBeInTheDocument();
+  });
+
   it('윤년(2/29) 반복일정 설정 시 4년에 한번 반복하도록 저장된다.', async () => {});
   it("1월 31일부터 '매월 31일' 반복 저장/ 종료일 5월 31일인 경우 31일이 있는 월인 1월, 3월, 5월만 저장된다.", async () => {});
   it("(필수아님)매월 '월말' 반복 저장 시 윤년의 29일에 저장된다.", async () => {});
@@ -37,8 +215,8 @@ describe('(필수) 반복 유형 선택', () => {
 });
 
 describe('(필수) 반복 간격 설정', () => {
-  it("2025-02-3부터 '3일마다 한번'씩 일정을 설정하고 종료일을 2/9일로 설정하는 경우 2/6, 2/9일에 일정이 등록된다.", async () => {});
-  it("2025-02-3일부터 '3주마다 한번'씩 일정을 설정하고 종료일을 2025-03-31 로 설정하는 경우 2/24, 3/17일에도 저장된다.", async () => {});
+  it("2025-02-03부터 '3일마다 한번'씩 일정을 설정하고 종료일을 2/9일로 설정하는 경우 2/6, 2/9일에 일정이 등록된다.", async () => {});
+  it("2025-02-03일부터 '3주마다 한번'씩 일정을 설정하고 종료일을 2025-03-31 로 설정하는 경우 2/24, 3/17일에도 저장된다.", async () => {});
   it("2025-02-03일부터 '2개월마다 한번'씩 일정을 설정하고 종료일을 2025-05-01 로 설정하는 경우 03-03, 04-03일에도 저장된다.", async () => {});
 });
 
